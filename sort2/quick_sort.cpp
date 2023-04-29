4d3fdeec121d6eec35033202bfe7484cb2dec159
Jan Vondruška <vondrj35@fjfi.cvut.cz>
2023-04-29 18:28:21 +0200

    radix sort

--- sort2/quick_sort.cpp
+++ sort2/quick_sort.cpp
@@ -1,6 +1,6 @@
 #include <utility>
 #include <stack>
-#include <cmath>
+#include <vector>
 
 void divide(int* data, int left, int right, int pivot)
 {
@@ -24,8 +24,44 @@ void divide(int* data, int left, int right, int pivot)
 }
 
 
+//Start of selection sort
+int smallest_key(int* arr, int right, int from)
+{
+	for (int i = from; i < right + 1; i++)
+	{
+		if (arr[i] < arr[from])
+		{
+			from = i;
+		}
+	}
+	return from;
+}
+
+void selection_sort(int* arr, int left, int right)
+{
+	for (int i = left; i < right; i++)
+	{
+		int smallest_index = smallest_key(arr, right, i);       //We find the index of the smallest element in the subarray starting by index i
+		if (smallest_index != i)
+		{
+			int help = arr[i];
+			arr[i] = arr[smallest_index];
+			arr[smallest_index] = help;
+		}
+	}
+}
+//
+
+
 void quick_sort_alg(int* data, int left, int right)
 {
+	if (right - left + 1 < 3)
+	{
+		selection_sort(data, left, right);
+		return;
+	}
+
+
 	int i = left;
 	int j = right;
 	int pivot = data[(left + right) / 2];
@@ -103,20 +139,21 @@ void quick_sort_alg_non_rec(int* data, int n)
 
 void quick_sort(int* data, int n)
 {
-	//quick_sort_alg(data, 0, n - 1);
-	quick_sort_alg_non_rec(data, n);
+	quick_sort_alg(data, 0, n - 1);
 }
+
+
 
 
 void shell_sort(int* data, int n)
 {
 	// Start with a big gap, then reduce the gap until it becomes 1
-	for (int gap = pow(2, (int)log2(200)) - 1; gap > 0; gap /= 2) 
+	for (int gap = pow(2, (int)log2(200)) - 1; gap > 0; gap /= 2)
 	{
 		// Do a gapped insertion sort for this gap size.
 		// The first gap elements a[0..gap-1] are already in gapped order
 		// keep adding one more element until the entire array is gap sorted
-		for (int i = gap; i < n; i++) 
+		for (int i = gap; i < n; i++)
 		{
 			// add a[i] to the elements that have been gap sorted
 			// save a[i] in temp and make a hole at position i
@@ -132,5 +169,40 @@ void shell_sort(int* data, int n)
 			// put temp (the original a[i]) in its correct location
 			data[j] = temp;
 		}
+	}
+}
+
+
+void radix_sort(int* data, int n)
+{
+	std::vector<std::vector<int>> fronty(10, std::vector<int>());
+
+	int z = 10;
+	int d = 1;
+
+	int x;
+	int j;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			x = data[i];
+			j = (x / d) % z;
+			fronty[j].push_back(x);
+		}
+		d = d * z;
+
+
+		int help = 0;
+		for (int k = 0; k < 10; k++)
+		{
+			for (int index = 0; index < fronty[k].size(); index++)
+			{
+				data[help] = fronty[k][index];
+				help++;
+			}
+			fronty[k].clear();
+		}
+
 	}
 }
--- sort2/quick_sort.h
+++ sort2/quick_sort.h
@@ -1,7 +1,14 @@
 #pragma once
 
 
+int smallest_key(int* arr, int right, int from);
+void selection_sort(int* arr, int left, int right);
+
 void divide(int* data, int left, int right, int pivot);
 void quick_sort_alg(int* data, int left, int right);
+void quick_sort_alg_non_rec(int* data, int n);
 void quick_sort(int* data, int n);
-void shell_sort(int* arr, int n);
+
+void shell_sort(int* data, int n);
+
+void radix_sort(int* data, int n);
